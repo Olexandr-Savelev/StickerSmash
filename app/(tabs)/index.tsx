8003cac7f3709d5e3fa043d3fe12965682b8bd6b
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-10-08 15:49:14 +0300

    add dom-to-image library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/UI/Button";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useRef, useState } from "react";
 import IconButton from "@/components/UI/IconButton";
@@ -12,6 +12,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { usePermissions } from "expo-media-library";
 import { captureRef } from "react-native-view-shot";
 import * as MediaLibrary from "expo-media-library";
+import domtoimage from "dom-to-image";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
@@ -56,18 +57,34 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-      await MediaLibrary.saveToLibraryAsync(localUri);
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
 
-      if (localUri) {
-        alert("Image saved successfully");
+        if (localUri) {
+          alert("Image saved successfully");
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
+    } else {
+      try {
+        var dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 1,
+          width: 320,
+          height: 440,
+        });
+        var link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -88,7 +105,6 @@ export default function Index() {
           />
         )}
       </View>
-
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
